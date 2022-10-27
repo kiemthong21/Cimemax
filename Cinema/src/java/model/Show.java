933aafd2696e408490e12ad0906c4590a9002ebe
thongnguyen2001 <81925625+thongnguyen2001@users.noreply.github.com>
2022-10-27 08:24:18 +0700

    update class

---
+++ Cinema/src/java/model/Show.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author int.thong.nk
+ */
+public class Show {
+    private int showId;
+    private  Room room;
+    private Film film;
+    private Date showDate;
+    private int Status;
+}
